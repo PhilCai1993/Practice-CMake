6ec398a4bf356e23539f07307cc7ac1f8d5e4d66
Phil <zheren.cai@ele.me>
2018-04-03 13:50:55 +0800

    Demo4 -- Configuration

---
+++ Demo4/CMakeFiles/DEMO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DEMO.dir/main.cc.o"
+  "DEMO.pdb"
+  "DEMO"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DEMO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
