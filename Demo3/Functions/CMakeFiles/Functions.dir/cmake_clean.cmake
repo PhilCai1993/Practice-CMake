9d2f7ef2729787c3668661c1b166ea712bf1a01a
Phil <zheren.cai@ele.me>
2018-04-03 13:30:04 +0800

    Demo3

---
+++ Demo3/Functions/CMakeFiles/Functions.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Functions.dir/MyFunctions.cc.o"
+  "libFunctions.pdb"
+  "libFunctions.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Functions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
